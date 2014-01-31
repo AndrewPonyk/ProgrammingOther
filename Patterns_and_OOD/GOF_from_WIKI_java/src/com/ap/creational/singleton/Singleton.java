2576c4d3e866a10a1ae7462282f9379d1bc02e94
AndrewPonyk <andrew9999@ukr.net>
2014-01-31 10:19:47 +0200

    Patterns add Singleton java code

--- Patterns_and_OOD/GOF_from_WIKI_java/src/com/ap/creational/singleton/Singleton.java
+++ Patterns_and_OOD/GOF_from_WIKI_java/src/com/ap/creational/singleton/Singleton.java
@@ -1,10 +1,29 @@
-package
-com.ap.creational.singleton;
- public enum Singleton {
-    INSTANCE;
-        public void execute (String arg) {
-                // perform operation here 
-        	System.out.println("executing something");
-        	
-        }
- }
+package com.ap.creational.singleton;
+
+//enum way Singleton
+/*public enum Singleton {
+	INSTANCE;
+	public void execute(String arg) {
+		// perform operation here
+		System.out.println("executing something");
+
+	}
+}*/
+
+public class Singleton{
+	private Singleton(){
+		System.out.println("Created singleton");
+	}
+	
+	private static class SingletonHolder{
+		public static final Singleton INSTANCE = new Singleton();
+	}
+	
+	public void execute(String arg){
+		System.out.println("Execute " + arg);
+	}
+	
+	public static Singleton getInstanse(){
+		return SingletonHolder.INSTANCE;
+	}
+}
--- Patterns_and_OOD/GOF_from_WIKI_java/src/com/ap/creational/singleton/SingletonProgram.java
+++ Patterns_and_OOD/GOF_from_WIKI_java/src/com/ap/creational/singleton/SingletonProgram.java
@@ -1,12 +1,21 @@
 package com.ap.creational.singleton;
 
+
 public class SingletonProgram {
 
 	public static void main(String[] args) {
 			System.out.println("Using Singleton");
-			Singleton singleton = Singleton.INSTANCE;
-			singleton.execute("some argument");
 			
+			///Enum way
+			//Singleton singleton = Singleton.INSTANCE;
+			//singleton.execute("some argument");
+			
+			
+			/// Initialization On Demand Holder Idiom
+			Singleton singleton = Singleton.getInstanse();
+			singleton.execute("123");
 	}
 
 }
+
+
