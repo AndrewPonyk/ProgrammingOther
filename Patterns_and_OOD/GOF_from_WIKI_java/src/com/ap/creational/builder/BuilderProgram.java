1829426e0f98f2c958a8aa5a29092e558e91f1b5
Andrew Ponyk <andrew9999@ukr.net>
2014-02-02 17:04:09 +0200

    GOF from wiki : builder pattern finish

--- Patterns_and_OOD/GOF_from_WIKI_java/src/com/ap/creational/builder/BuilderProgram.java
+++ Patterns_and_OOD/GOF_from_WIKI_java/src/com/ap/creational/builder/BuilderProgram.java
@@ -1,5 +1,11 @@
 package com.ap.creational.builder;
 
 public class BuilderProgram {
-
+	public static void main(String[] args) {
+		System.out.println("Using Builder pattern");
+		
+		User andrew = new  User.UserBuilder("andrew","lastname").age(21).build();
+		
+		System.out.println(andrew.getFirstName() + " " + andrew.getAge());
+	}
 }
