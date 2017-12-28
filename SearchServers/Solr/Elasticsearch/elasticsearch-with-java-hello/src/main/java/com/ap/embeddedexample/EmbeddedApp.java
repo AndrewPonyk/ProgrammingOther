b496fb2ed6724346e91c392b3e0d9cfd7791fa7c
AndrewPonyk <andrew9999@ukr.net>
2017-12-28 14:49:58 +0200

    elasticsearch embedded worksgit status!

--- SearchServers/Solr/Elasticsearch/elasticsearch-with-java-hello/src/main/java/com/ap/embeddedexample/EmbeddedApp.java
+++ SearchServers/Solr/Elasticsearch/elasticsearch-with-java-hello/src/main/java/com/ap/embeddedexample/EmbeddedApp.java
@@ -5,6 +5,7 @@ import pl.allegro.tech.embeddedelasticsearch.IndexSettings;
 import pl.allegro.tech.embeddedelasticsearch.PopularProperties;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by andrii on 28.12.17.
@@ -13,10 +14,11 @@ public class EmbeddedApp {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("test embedded EL");
         final EmbeddedElastic embeddedElastic = EmbeddedElastic.builder()
-                .withElasticVersion("5.0.0")
-                .withSetting(PopularProperties.TRANSPORT_TCP_PORT, 9350)
+                .withElasticVersion("6.0.0")
+                .withSetting(PopularProperties.TRANSPORT_TCP_PORT, 9351)
                 .withSetting(PopularProperties.CLUSTER_NAME, "my_cluster")
-                .withSetting(PopularProperties.HTTP_PORT, 9200)
+                .withSetting(PopularProperties.HTTP_PORT, 9201)
+                .withStartTimeout(100, TimeUnit.SECONDS)
                 //.withPlugin("analysis-stempel")
 //                .withIndex("cars", IndexSettings.builder()
 //                        .withType("car", getSystemResourceAsStream("car-mapping.json"))
