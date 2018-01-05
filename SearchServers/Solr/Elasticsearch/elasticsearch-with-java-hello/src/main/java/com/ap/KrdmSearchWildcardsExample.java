0f4d51c3d92f6e6b79443defa03f836956f6a734
ponykand <Andrii.Ponyk@externals.adidas.com>
2018-01-05 17:03:26 +0200

    elastic search examples added

--- SearchServers/Solr/Elasticsearch/elasticsearch-with-java-hello/src/main/java/com/ap/KrdmSearchWildcardsExample.java
+++ SearchServers/Solr/Elasticsearch/elasticsearch-with-java-hello/src/main/java/com/ap/KrdmSearchWildcardsExample.java
@@ -1,55 +1,53 @@
 package com.ap;
 
 import org.apache.http.HttpHost;
-import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public class KrdmSearchExample {
+public class KrdmSearchWildcardsExample {
     public static void main(String[] args) throws IOException {
         System.out.println("Elaticsearch integration with java");
         RestHighLevelClient client = null;
 
+        System.out.println("*one*two*".replaceAll("\\*", "\\* \\*") +"|");
+        System.out.println(Arrays.toString("*one*two*".replaceAll("\\*", "\\* \\*").split("\\s")));
         try {
             client = new RestHighLevelClient(
                     RestClient.builder(
-                            new HttpHost("localhost", 9900, "http"),
-                            new HttpHost("localhost", 9800, "http"),
                     new HttpHost("localhost", 9200, "http")));
-            SearchRequest searchRequest = new SearchRequest("kvm1");
+            SearchRequest searchRequest = new SearchRequest("temp");
             SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 
+            //QueryStringQueryBuilder queryBuilder = QueryBuilders.queryStringQuery("10*0 t*re* tw*").defaultOperator(Operator.AND);
+            //MultiMatchQueryBuilder queryBuilder = QueryBuilders.multiMatchQuery("t*re* tw* 10*0", "name" );
+            //simple wildcard query
             BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+            //queryBuilder.must(QueryBuilders.queryStringQuery("t*re* tw* 100").defaultOperator(Operator.AND));
 
-            //simple query
-            queryBuilder.must(QueryBuilders.wildcardQuery("vendorCode", "* 101*"));
-            //queryBuilder.must(QueryBuilders.wildcardQuery("vcClassCode", "*1"));
+            //simple wildcard query
+            //queryBuilder.must(QueryBuilders.queryStringQuery("t*re* tw* 100").defaultOperator(Operator.AND));
 
-            // nested wildcard query
-            //queryBuilder.must(QueryBuilders.nestedQuery("data", QueryBuilders.wildcardQuery("data.Desc", "*jun*"), ScoreMode.Avg));
+            // AND operator query
+            //queryBuilder.must(QueryBuilders.matchQuery("name", "one two").operator(Operator.OR));
+
+            // minimumShouldMatch query
+            //queryBuilder.must(QueryBuilders.matchQuery("name", "one two three 130").minimumShouldMatch("75%"));
 
 
-            //sourceBuilder.from(0);
-            //sourceBuilder.size(5);
-            //sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
             sourceBuilder.size(50);
             sourceBuilder.query(queryBuilder);
             searchRequest.source(sourceBuilder);
-
-
 
             SearchResponse search = client.search(searchRequest);
 
@@ -57,9 +55,6 @@ public class KrdmSearchExample {
                 System.out.println(item.getSourceAsString());
                 System.out.println("========");
             }
-
-            List<KrdmDto> results = null;
-            List<SearchHit> collect = StreamSupport.stream(search.getHits().spliterator(), false).map(item -> item).collect(Collectors.toList());
         } finally {
             client.close();
         }
--- SearchServers/Solr/Elasticsearch/elasticsearch-with-java-hello/src/main/java/com/ap/embeddedexample/EmbeddedApp.java
+++ SearchServers/Solr/Elasticsearch/elasticsearch-with-java-hello/src/main/java/com/ap/embeddedexample/EmbeddedApp.java
@@ -4,25 +4,39 @@ import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
 import pl.allegro.tech.embeddedelasticsearch.IndexSettings;
 import pl.allegro.tech.embeddedelasticsearch.PopularProperties;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.ClassLoader.getSystemResourceAsStream;
 
 /**
  * Created by andrii on 28.12.17.
  */
 public class EmbeddedApp {
-    public static void main(String[] args) throws IOException, InterruptedException {
+
+    public static void main(String[] args) {
+        try {
+            new EmbeddedApp().test();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void test() throws IOException, InterruptedException {
         System.out.println("test embedded EL");
         final EmbeddedElastic embeddedElastic = EmbeddedElastic.builder()
-                .withElasticVersion("6.0.0")
+                .withElasticVersion("6.1.1")
                 .withSetting(PopularProperties.TRANSPORT_TCP_PORT, 9351)
                 .withSetting(PopularProperties.CLUSTER_NAME, "my_cluster")
-                .withSetting(PopularProperties.HTTP_PORT, 9201)
+                .withSetting(PopularProperties.HTTP_PORT, 9200)
                 .withStartTimeout(100, TimeUnit.SECONDS)
                 //.withPlugin("analysis-stempel")
-//                .withIndex("cars", IndexSettings.builder()
-//                        .withType("car", getSystemResourceAsStream("car-mapping.json"))
-//                        .build())
+                .withIndex("krdm", IndexSettings.builder()
+                        .withType("doc", getSystemResourceAsStream("krdm.json"))
+                        .build())
 //                .withIndex("books", IndexSettings.builder()
 //                        .withType(PAPER_BOOK_INDEX_TYPE, getSystemResourceAsStream("paper-book-mapping.json"))
 //                        .withType("audio_book", getSystemResourceAsStream("audio-book-mapping.json"))
@@ -30,5 +44,37 @@ public class EmbeddedApp {
 //                        .build())
                 .build()
                 .start();
+
+
+        //Thread.sleep(10000);
+
+        //System.out.println("killing embedded elasticsearch");
+        //embeddedElastic.stop();
+    }
+
+
+    private String getFile(String fileName) {
+
+        StringBuilder result = new StringBuilder("");
+
+        //Get file from resources folder
+        ClassLoader classLoader = EmbeddedApp.class.getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        try (Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+
     }
 }
