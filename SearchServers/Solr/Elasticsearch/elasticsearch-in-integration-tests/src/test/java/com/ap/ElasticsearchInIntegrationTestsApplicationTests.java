03c7a84c3050d16081f2bdb5227974a82aa73238
ponykand <Andrii.Ponyk@externals.adidas.com>
2018-01-11 11:23:30 +0200

    maven+docker+elasticsearch - working example

--- SearchServers/Solr/Elasticsearch/elasticsearch-in-integration-tests/src/test/java/com/ap/ElasticsearchInIntegrationTestsApplicationTests.java
+++ SearchServers/Solr/Elasticsearch/elasticsearch-in-integration-tests/src/test/java/com/ap/ElasticsearchInIntegrationTestsApplicationTests.java
@@ -1,6 +1,8 @@
 package com.ap;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.Wait;
+import org.testcontainers.shaded.org.apache.http.HttpResponse;
+import org.testcontainers.shaded.org.apache.http.client.HttpClient;
+import org.testcontainers.shaded.org.apache.http.client.methods.HttpPut;
+import org.testcontainers.shaded.org.apache.http.entity.ContentType;
+import org.testcontainers.shaded.org.apache.http.entity.StringEntity;
+import org.testcontainers.shaded.org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,9 +35,21 @@ public class ElasticsearchInIntegrationTestsApplicationTests {
 					.withEnv("network.host", "_site_")
 					.withEnv("network.publish_host", "_local_");
 
-	@Test
-	public void contextLoads() {
-		Assert.assertTrue(true);
+	@BeforeClass
+	public static void setup() throws IOException {
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPut request = new HttpPut("http://localhost:9200/kvm");
+		request.setHeader("Content-Type", "application/json");
+
+		String payload = FileUtils.readFileToString(new File("src/test/resources/elasticsearch_mappings/kvm.json"), "UTF-8");
+		StringEntity entity = new StringEntity(payload, ContentType.APPLICATION_FORM_URLENCODED);
+		request.setEntity(entity);
+
+		HttpResponse response = httpClient.execute(request);
 	}
 
+	@Test
+	public void contextLoads() {
+		Assert.assertTrue(false);
+	}
 }
